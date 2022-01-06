ab4b72b836c9c03f7665c3eaf790c94e0b4c88a1
Marcin Szwed <49093128+szwedm@users.noreply.github.com>
2022-01-06 12:01:09 +0100

    Add error handling in auth service

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { BehaviorSubject, Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { BehaviorSubject, Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
 import { User } from '../models/user';
 
 const httpOptions = {
@@ -17,11 +17,26 @@ export class AuthService {
   }
 
   signin(username: string, password: string): Observable<User> {
-    return this.http.post<User>('http://localhost:8080/signin', {username, password}, httpOptions);
+    return this.http.post<User>('http://localhost:8080/signin', {username, password}, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
   }
 
   signup(username: string, password: string, role: string) {
-    return this.http.post('http://localhost:8080/users', {username, password, role}, httpOptions);
+    return this.http.post('http://localhost:8080/users', {username, password, role}, httpOptions)
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.status === 0) {
+      console.error('An error occurred: ', error.error);
+    } else {
+      console.error(`Backend returned code ${error.status}, body was: `, error.error);
+    }
+    return throwError(() => new Error('Request error!'));
   }
 
 }
